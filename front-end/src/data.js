33d2d15fce7a842e0466a6a4288892f1d2ee4de3
hgrcode <harrisramsay@hotmail.co.uk>
2022-01-02 19:19:54 +0000

    timeline for code venture complete awaits data

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -28,6 +28,152 @@ const data = [
         }
 
     },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+
+    {
+        text: 'we nkjn oi oij oijoij jo joijgjh jhbhjbj bhjbj bjhn',
+        date: '00/00/00',
+        category:{
+            tag: 'medium',
+            color: '#018f69'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'my first project',
+        date: '21/12/21',
+        category:{
+            tag: 'medium',
+            color: '#018f69'
+        },
+        link:{
+            url:
+            'localhost:3000/',
+            text: 'Check out here for more >'
+        }
+
+    },
+
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
+    {
+        text: 'First finished project upload',
+        date: '27/12/21',
+        category:{
+            tag: 'medium',
+            color: '#e17b77'
+        },
+        link:{
+            url:'https://github.com/hgrcode?tab=repositories',          
+            text: 'check out here for more >'
+        }
+
+    },
 
     {
         text: 'we nkjn oi oij oijoij jo joijgjh jhbhjbj bhjbj bjhn',
